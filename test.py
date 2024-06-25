65e8047fb253c0f502e99a4beca3c8599f18c43d
Abzal Nashauov <Abzal.Nashauov@kcell.kz>
2024-06-25 20:02:58 +0500

    First Commit

--- test.py
+++ test.py
@@ -1,71 +1,58 @@
-# stroka1 = 'пара-ра-рам рам-пам-папам па-ра-па-дам'
-# vowels = ['а', 'е', 'ё', 'и', 'й', 'о', 'у', 'ы', 'э', 'ю', 'я']
-# phrases = stroka1.split()
-# if len(phrases) < 2:
-#     print('Количество фраз должно быть больше одной!')
-# else:
-#     countVowels = []
+def phonebook():
+    choise = show_menu()
+    phonebook = read_file('phonebook.txt')
+    
 
-# for i in phrases:
-#     countVowels.append(len([x for x in i if x.lower() in vowels]))
-
-# if countVowels.count(countVowels[0]) == len(countVowels):
-#     print('Парам пам-пам')
-# else:
-#     print('Пам парам')
-
-# cities = 'Шымкент Алматы Астана Уфа'.split()
-# res_cities = [ 1 for _ in cities if len(_) > 5]
-# print(*res_cities)
-N = 5
-list = [[1 if i == j else 0 for j in range(N)] for i in range(N)]
-for i in list:
-    print(*i)
+def show_menu():
+    print("\nВыберите пункт меню для работы со справочником\n"
+          "1.\n" 
+          "2.\n"
+          "3.\n"
+          "4.\n"
+          "5.\n"
+          "6.")
+    choise = int(input())
+    return choise
 
 
-
-list2 = [[1 if i == j else 0 for j in range(N)] for i in range(N)]
-
-
-s = '-8 15 20 126 -40'
-
-print(*filter(lambda x: 9 < abs(int(x)) < 100, s.split()))
-
-
-
+def read_file(filename):
+    phonebook = []
+    fields = ['Фамилия', 'Имя', 'Телефон', 'Описание']
+    with open(filename, 'r', encoding='utf-8') as phone_read:
+        for line in phone_read:
+            record = dict(zip(fields, line.split('')))
+            phonebook.append(record)
+    return phonebook
 
 
+def write_file(filename, phonebook):
+    s = ''
+    with open(filename, 'w') as phone_write:
+        for i in range(len(phonebook)):
+            for v in phonebook[i].values():
+                s = s + v + ','
+            phone_write.write(f'{s[:-1]}\n')
 
 
+def copy_line(in_file, out_file):
+    print("Введите номер строки:")
+    try:
+        line_num = int(input()) + 1
+    except:
+        'Введите число'
+    try:
+        with open(in_file, 'r', encoding='utf-8') as file_read:
+            lines = file_read.readlines()
+            
+            if 0 <= line_num < len(lines):
+                line_to_copy = lines[line_num]
+                with open(out_file, 'a', encoding='utf-8') as f_target:
+                    f_target.write(line_to_copy)
+                print(f"Строка номер {line_num + 1} успешно добавлена в файл {target_file}")
+            else:
+                print("Неверный номер строки. Выберите число между 1 и {len(lines)}")
+    except:
+        "Не найдены файлы"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-list12 = [[1 if i == j else 0 for j in range(N)] for i in range(N)]
-
-
-s = '10 18 -20 1 99 100 160'
-print( *filter(lambda x: len(str(abs(int(x)))) == 2,  s.split()))
-
-
-
-
-
-
-list1 = [[1 if i == j else 0 for j in range(N)] for i in range(N)]
-print(*list12)
-
-
-
+if __name__ == "__main__":
+    phonebook()
